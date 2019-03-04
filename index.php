12b2633eaef30d24c93934c0413167fbf4cd93c3
miniral199 <miniral199@gmail.com>
2019-03-04 18:23:58 +1000

    index.php file

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <div id = "container">
+    <div id = "pab">CLICK ON ME</div>
+        <div id = "book">
+            <div id = "name">
+        <b><i>Book</i></b>
+</div>
+            <div class = "page"></div>
+        </div>
+</div>
+<script src = "script.js">
+</script>
+</body>
+</html>
